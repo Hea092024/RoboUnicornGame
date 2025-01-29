675ae1b12bf94e38ee4f0cfe09fb60c34c93a4d9
Hector Acevedo <HEA092024@gmail.com>
2025-01-29 12:35:37 +0100

    linked js to html

---
+++ game.js
@@ -0,0 +1,57 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Unicorn {
+  constructor() {
+    this.x = 50;
+    this.y = canvas.height - 100;
+    this.width = 80;
+    this.height = 50;
+    this.velocityY = 0;
+    this.gravity = 1;
+    this.jumpPower = -15;
+    this.jumping = false;
+  }
+
+  jump() {
+    if (!this.jumping) {
+      this.velocityY = this.jumpPower;
+      this.jumping = true;
+    }
+  }
+
+  update() {
+    this.y += this.velocityY;
+    this.velocityY += this.gravity;
+
+    if (this.y >= canvas.height - 100) {
+      this.y = canvas.height - 100;
+      this.jumping = false;
+    }
+  }
+
+  draw() {
+    ctx.fillStyle = "cyan";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+const unicorn = new Unicorn();
+
+function update() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  unicorn.update();
+  unicorn.draw();
+  requestAnimationFrame(update);
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Space") {
+    unicorn.jump();
+  }
+});
+
+update();
